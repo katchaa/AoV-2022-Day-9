1bd7f739dfd35e7679a144c5423f4ef2eb2e9d5a
Katcha <kratochvilova93@email.cz>
2022-12-12 21:53:27 +0100

    Task solution

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div class="w-full h-full p-4 flex justify-center items-center">
-    <div class="max-w-md">
+    <div class="max-w-md flex flex-col items-center">
       <div>
         <img src="/assets/tree.svg" alt="Christmas tree" />
       </div>
       <div class="mt-2 flex justify-center items-center">
         <img
-          v-for="present in presents"
+          v-for="present in sortPresents"
           :key="present.id"
           :src="present.src"
           :alt="`Present ${present.id}`"
           data-qa="present"
         />
       </div>
+      <button
+        @click="toggleSort()"
+        class="bg-indigo-600 text-white px-3 py-2 mt-5 rounded-lg border border-black hover:bg-indigo-500 transition duration-200"
+      >
+        Toggle sort
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref, computed, reactive } from 'vue'
 import presents from './presents.json'
+
+const sortMethods = reactive({
+  sorted: (x, y) => x.dimensions.width * x.dimensions.height - y.dimensions.width * y.dimensions.height,
+  default: (x, y) => x.id - y.id,
+})
+
+let activeSortMethod = ref('default')
+
+const toggleSort = () => {
+  activeSortMethod.value = activeSortMethod.value === 'default' ? 'sorted' : 'default'
+}
+
+const sortPresents = computed(() => presents.sort(sortMethods[activeSortMethod.value]))
 </script>
